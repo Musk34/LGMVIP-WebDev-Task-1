4a1340506950f9c92040d213b25b7d21dc677b83
Muskan Singh <singhmus13@gmail.com>
2021-10-27 21:24:40 +0530

    first commit

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 "# LGMVIP-WebDev-Task-1" 
 "# LGMVIP-WebDev-Task-1" 
 "# LGMVIP-WebDev-Task-1" 
+"# LGMVIP-WebDev-Task-1" 
---
+++ js/gallery.js
@@ -0,0 +1,30 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var j;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (j = 0; j < slides.length; j++) {
+    slides[j].style.display = "none";
+  }
+  for (j = 0; j < dots.length; j++) {
+    dots[j].className = dots[j].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
